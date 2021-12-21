d898e0a50e7fd4464db54c1c06a87a2784e9b1f3
kevinamexo <bangitskev@gmail.com>
2021-12-21 15:36:36 +0000

    updateProduct controller

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -35,7 +35,31 @@ export async function updateProductHandler(
   req: Request<UpdateProductType["params"], any, UpdateProductType["body"]>,
   res: Response
 ) {
-  console.log(req);
+  try {
+    const userId = res.locals.user._id;
+    const productId = req.params.productId;
+    const update = req.body;
+
+    const product = await findProduct({ productId });
+
+    if (!product) {
+      return res.sendStatus(404);
+    }
+
+    if (product.user !== userId) {
+      return res.sendStatus(403);
+    }
+    const updatedProduct = await findAndUpdateProduct({ productId }, update, {
+      new: true,
+    }); //new returns modified document
+
+    return res.send(updatedProduct);
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      message: "Error updating Finding and Updating product",
+    });
+  }
 }
 
 export async function deleteProductHandler(
